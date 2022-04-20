02319faf655717a04bf7c6bd43de11df02524835
Ana Carolina <anacarolinamaciel15@gmail.com>
2022-04-20 23:36:21 +0100

    Add months names to calendar

--- SecondLesson/Calendar.java
+++ SecondLesson/Calendar.java
@@ -6,10 +6,36 @@ public class Calendar {
 		Scanner scannerObject= new Scanner(System.in);
 		int monthNumber= scannerObject.nextInt();
 
-		if (monthNumber <1 || monthNumber >12) {
-			System.out.println(monthNumber + " is an invalid month");
+		if (monthNumber >=1 && monthNumber <=12) {
+			String month= "";			
+			if (monthNumber == 1) {
+				month= "January";
+			} else if (monthNumber == 2) {
+				month= "February";
+			} else if (monthNumber == 3) {
+				month= "March";
+			} else if (monthNumber == 4) {
+				month= "April";
+			} else if (monthNumber == 5) {
+				month= "May";
+			} else if (monthNumber == 6) {
+				month= "June";
+			} else if (monthNumber == 7) {
+				month= "July";
+			} else if (monthNumber == 8) {
+				month= "August";
+			} else if (monthNumber == 9) {
+				month= "September";
+			} else if (monthNumber == 10) {
+				month= "October";
+			} else if (monthNumber == 11) {
+				month= "November";
+			} else {
+				month= "December";
+			}
+			System.out.println("choosed month is: "+ month);
 		} else {
-			System.out.println("Month choosed is: " + monthNumber);System.out.print ("valid month");
+			System.out.println(monthNumber + " is an invalid month");
 		}
 	}
 }
