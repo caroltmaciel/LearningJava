f5d38916be1d2fc82c68fac33a1c90633ebb4a0b
Ana Carolina <anacarolinamaciel15@gmail.com>
2022-04-20 22:21:46 +0100

    add first lesson

---
+++ FirstLesson/Types.java
@@ -0,0 +1,11 @@
+public class Types {
+	public static void main (String[] args){
+		String name= "Snoop Dogg";
+		int age= 69;
+		float height= 1.93F;
+		boolean smoker= true;
+		char sex= 'm';
+		double amountSpendOnMJ= 420.20;
+		long cigarreteSmoked= 420420420420;
+	}
+}
---
+++ FirstLesson/UsingArgs.java
@@ -0,0 +1,5 @@
+public class UsingArgs {
+	public static void main (String[] args) {
+			System.out.print("Hi, my name is: " + args[0]);
+	}
+}
